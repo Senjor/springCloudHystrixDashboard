8baf774b03808fbcee9976ad72a50060eeeb35aa
Senjor <377844325@qq.com>
2019-11-23 09:15:21 +0800

    add hystirx

---
+++ server-feign/src/main/java/com/zhiyou100/serverfeign/HelloServiceHystirx.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C), 2009-2019, 智游集团
+ * FileName: HelloServiceHystirx
+ * Author:   laosun
+ * Date:     2019/11/23 8:55 上午
+ * Description:
+ */
+package com.zhiyou100.serverfeign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloServiceHystirx  implements HelloService{
+
+
+    @Override
+    public String sayHello(String name) {
+        return name + "error";
+    }
+}
--- service-ribbon/src/main/java/com/zhiyou100/serverribbon/ServerRibbonApplication.java
+++ service-ribbon/src/main/java/com/zhiyou100/serverribbon/ServerRibbonApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @ComponentScan("com.zhiyou100")
+@EnableHystrix
 public class ServerRibbonApplication {
 
     @Bean
