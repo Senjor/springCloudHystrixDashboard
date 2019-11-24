1bbb5b230eb6a1abd2107c2b957ddea1e06f9e95
Senjor <377844325@qq.com>
2019-11-24 18:54:50 +0800

    add config

---
+++ config-client/src/test/java/com/zhiyou100/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiyou100.configclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
