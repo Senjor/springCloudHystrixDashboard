d2ee1f5eb4a98248ce79731415181607639f3335
Senjor <377844325@qq.com>
2019-11-27 16:48:09 +0800

    add Hystrix Dashboard

--- server-hello/src/main/java/com/zhiyou100/serverhello/ServerHelloApplication.java
+++ server-hello/src/main/java/com/zhiyou100/serverhello/ServerHelloApplication.java
@@ -1,29 +1,59 @@
 package com.zhiyou100.serverhello;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 @RestController
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+
 public class ServerHelloApplication {
 
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hello.action")
+    @HystrixCommand(fallbackMethod = "error")
+
     public  String sayHello(@RequestParam(value = "name",defaultValue = "xiaoming") String name){
 
         return "my name is:" + name +", port is : " + port;
+    }
+
+    public String error(String name){
+        return "sorry error";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerHelloApplication.class, args);
     }
 
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
