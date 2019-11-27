301562596972b9a33c1aaa8fb17a84a30790937a
Senjor <377844325@qq.com>
2019-11-27 14:38:40 +0800

    add HA eureka server

---
+++ eureka-server2/src/test/java/com/zhiyou100/eurekaserver2/EurekaServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiyou100.eurekaserver2;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
